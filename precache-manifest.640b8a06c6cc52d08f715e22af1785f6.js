880cfaf9d3c59f37a3b45aeec7003196a10c10c3
iamSK77 <sk77.mobilefix@gmail.com>
2020-07-22 14:10:12 +0530

    Updates

---
+++ precache-manifest.640b8a06c6cc52d08f715e22af1785f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e1cec9a38c7f81944b06f7b18a87e38",
+    "url": "/Contact_cards-react/index.html"
+  },
+  {
+    "revision": "1a97b1dd766e1d24d8cb",
+    "url": "/Contact_cards-react/static/js/2.8503bbe3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Contact_cards-react/static/js/2.8503bbe3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0692b78d6475b94f4d0",
+    "url": "/Contact_cards-react/static/js/main.a3b3e835.chunk.js"
+  },
+  {
+    "revision": "28e2865efa17fac3440c",
+    "url": "/Contact_cards-react/static/js/runtime-main.a2316b8f.js"
+  }
+]);
